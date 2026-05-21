760f2e082ba1c08de9045bc1d25e18ea4fb6b6e6
Minwoo Shim <65289061+hsminu@users.noreply.github.com>
2026-05-21 23:14:28 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
-enum State {Idle, RUNNING, PAUSED, DOME}
-
-void print_state(enum State s) {
-  switch(s) {
-case IDLE:
-    printf("State: Idle\n");
-    break;
-case RUNNING:
-    printf("State: RUNNING\n");
-    break;
-  }
+typedef struct  {
+  int x;
+  int y;
+} Point;
 
 int main() {
-  print_state(RUNNING);
+  Point p = {1, 2};
+
+  printf("%d %d", p.x, p.y);
   return 0;
 }
